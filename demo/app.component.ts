153ec847bebc13ee30ea74d40e30b1ae54b5a439
Pablo Reyes <pablo@srtk.com>
2017-11-15 11:04:14 -0300

    jsonapiCore added to example

--- demo/app.component.ts
+++ demo/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JsonapiCore } from 'ngx-jsonapi';
 
 @Component({
     selector: 'demo-app',
@@ -6,5 +7,22 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './app.component.html'
 })
 export class AppComponent /* implements OnInit */ {
+    public loading = '';
 
+    public constructor(
+        private jsonapiCore: JsonapiCore
+    ) {
+        jsonapiCore.loadingsStart = (): void => {
+            this.loading = 'LOADING...';
+        };
+        jsonapiCore.loadingsDone = (): void => {
+            this.loading = '';
+        };
+        jsonapiCore.loadingsOffline = (error): void => {
+            this.loading = 'No connection!!!';
+        };
+        jsonapiCore.loadingsError = (error): void => {
+            this.loading = 'No connection 2!!!';
+        };
+    }
 }
