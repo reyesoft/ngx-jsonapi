f4a6c65fedcc55d8d4ec5a8b98f0798076f17584
Pablo Reyes <pablo@srtk.com>
2017-12-16 13:58:27 -0300

    some spaces removed

---
+++ build/deploy-demo.ts
@@ -0,0 +1,14 @@
+var path = require('path');
+var ghpages = require('gh-pages');
+
+var dir = path.resolve(path.join(__dirname, '../', 'demo-dist'));
+ghpages.publish(dir, {
+    user: {
+        name: 'Pablo Reyes',
+        email: 'pablo@reyesoft.com',
+    },
+    message: '(deploy): CI',
+    logger: function(message) {
+        console.log('gh-pages: ', message);
+    },
+});
