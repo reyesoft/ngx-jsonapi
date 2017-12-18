88da4d8ae1cf5320c956da26b4fb8a6762ffbb5c
Pablo Reyes <pablo@srtk.com>
2017-12-17 21:54:43 -0300

    test.js fixed

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
             'text/x-typescript': ['ts', 'tsx']
         },
         coverageIstanbulReporter: {
-            reports: ['html', 'lcovonly', 'text-summary' ],
+            reports: ['html', 'lcovonly', 'text-summary'],
             fixWebpackSourcePaths: true
         },
         angularCli: {
