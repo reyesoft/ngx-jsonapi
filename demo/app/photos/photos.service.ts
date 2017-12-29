4285a088ea1fd94cf24f422c13bb61fbc0a384c7
Pablo Reyes <pablo@srtk.com>
2017-12-29 17:33:42 -0300

    version v1 bump. headers added (#24)
    * version v1 bump. headers added
    
    * small fix photos services

--- demo/app/photos/photos.service.ts
+++ demo/app/photos/photos.service.ts
@@ -3,15 +3,16 @@ import { Service, ISchema, Resource } from 'ngx-jsonapi';
 
 @Injectable()
 export class PhotosService extends Service {
+    public resource = Photo;
     public type = 'photos';
 }
 
 export class Photo extends Resource {
     public attributes: {
-        title: {},
-        uri: {},
-        imageable_id: {},
-        created_at: {},
-        updated_at: {}
+        title: string,
+        uri: string,
+        imageable_id: string,
+        created_at: string,
+        updated_at: string
     };
 }
