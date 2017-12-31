d105104e00ee611191ca313c1b6d0b464dcda0b0
Umbert <umbertix@gmail.com>
2017-12-31 16:22:03 +0000

    Add Accept header with the type. (#31)
    * Add Accept header with the type.
    
    In order to follow the standard send the accept header with the type. 
    Api documentation reference: http://jsonapi.org/format/#fetching-resources
    
    * indentation fix

--- src/sources/http.service.ts
+++ src/sources/http.service.ts
@@ -47,7 +47,10 @@ export class Http {
                 this.rsJsonapiConfig.url + path,
                 data || null,
                 {
-                    headers: new HttpHeaders({ 'Content-Type': 'application/vnd.api+json' })
+                    headers: new HttpHeaders({
+                        'Content-Type': 'application/vnd.api+json',
+                        'Accept': 'application/vnd.api+json'
+                    })
                 }
             );
 
