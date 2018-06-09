7e212a558bcd31e0baa619ea88ad375eabb1b0c8
Pablo Reyes <pablo@srtk.com>
2018-06-08 21:56:55 -0300

    @todo added.

--- src/decorators/autoregister.ts
+++ src/decorators/autoregister.ts
@@ -1,6 +1,7 @@
 import { Service } from '../service';
 
 export function Autoregister() {
+    // @todo specify the correct types `return function<T extends typeof Service>(target: T): T`
     return function(target: any): any {
         // save a reference to the original constructor
         var original = target;
