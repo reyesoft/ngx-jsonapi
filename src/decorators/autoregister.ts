fd173be19e68c4b7df74309349e8e84f43085e02
Maxi <maxi7587@hotmail.com>
2018-07-16 16:33:15 -0300

    @Autoregister fixed (#88)
    * started testing
    
    * started testing
    
    * testing
    
    * testing
    
    * improved ngx-jsonapi.module test
    
    * improved ngx-jsonapi.module test
    
    * improved ngx-jsonapi.module test
    
    * improved ngx-jsonapi.module test
    
    * working
    
    * fixed autoregister
    
    * removed console.log

--- src/decorators/autoregister.ts
+++ src/decorators/autoregister.ts
@@ -1,23 +1,24 @@
 import { Service } from '../service';
 
 export function Autoregister() {
-    // @todo specify the correct types `return function<T extends typeof Service>(target: T): T`
-    return (target: any): any => {
-        // save a reference to the original constructor
-        let original = target;
+    return (target): any => {
+        const original = target;
 
-        // the new constructor behaviour
-        let f: any = function(...args) {
-            let instance = original.apply(this, args);
+        /*tslint:disable: only-arrow-functions*/
+        // function name is required
+        const newConstructor: any = function newCtor(...args) {
+            const c: any = function childConstuctor() {
+                return original.apply(this, arguments);
+            };
+            c.prototype = Object.create(original.prototype);
+            const instance = new c(...args);
             instance.register();
 
             return instance;
         };
 
-        // copy prototype so intanceof operator still works
-        f.prototype = original.prototype;
+        newConstructor.prototype = Object.create(target.prototype);
 
-        // return new constructor (will override original)
-        return f;
+        return newConstructor;
     };
 }
