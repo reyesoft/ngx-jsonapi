98361c4850071103e1057303fe3e3dcfe71b1bd0
Pablo Reyes <pablo@srtk.com>
2018-08-12 23:13:56 -0300

    Eager relationships fixed (#101)
    * fix on $toArray().
    * Better message on don't registered service.
    * Demo PhotoService fixed.
    * demo: photoservice fixed on author-component
    * version bump 1.1.2. Closes #68

--- demo/app/photos/photos.service.ts
+++ demo/app/photos/photos.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Service, ISchema, Resource } from 'ngx-jsonapi';
+import { Service, ISchema, Resource, Autoregister } from 'ngx-jsonapi';
 
 @Injectable()
+@Autoregister()
 export class PhotosService extends Service {
     // public resource = Photo;
     public type = 'photos';
