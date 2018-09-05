d09fc51af0e48cf51a52c2a8006f18d8891cc509
Pablo Reyes <pablo@srtk.com>
2018-09-05 19:15:18 -0300

    small fixes

--- demo/app/authors/components/author.component.ts
+++ demo/app/authors/components/author.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Resource } from 'ngx-jsonapi';
 import { PhotosService } from '../../photos/photos.service';
