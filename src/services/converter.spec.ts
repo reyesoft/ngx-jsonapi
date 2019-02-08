55cafd9ea298e7bf4a58427981daeed54e4acb52
maxi7587 <maxi7587@hotmail.com>
2019-02-08 10:49:56 -0300

    lib tests passing

--- src/services/converter.spec.ts
+++ src/services/converter.spec.ts
@@ -1,6 +1,27 @@
 import { Converter } from './converter';
+import { Core } from '../core';
 import { IDataResource } from '../interfaces/data-resource';
 import { IResourcesByType } from 'ngx-jsonapi';
+
+import { JsonapiConfig } from 'src/jsonapi-config';
+import { StoreService as JsonapiStore } from '../sources/store.service';
+import { Http as JsonapiHttpImported } from '../sources/http.service';
+import { HttpClient, HttpHandler, HttpRequest, HttpEvent, HttpResponse } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+class HttpHandlerMock implements HttpHandler {
+    public handle(req: HttpRequest<any>): Observable<HttpEvent<any>> {
+        let subject = new BehaviorSubject(new HttpResponse());
+
+        return subject.asObservable();
+    }
+}
+
+let core = new Core(
+    new JsonapiConfig(),
+    new JsonapiStore(),
+    new JsonapiHttpImported(new HttpClient(new HttpHandlerMock()), new JsonapiConfig())
+);
 
 describe('Converter', () => {
     it('json_array2resources_array_by_type(array) should be converted to IResourcesByType', () => {
