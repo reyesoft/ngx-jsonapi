b6797cbd50e005a05a7d1eab2781be2ca5df8ad9
maxi7587 <maxi7587@hotmail.com>
2019-02-18 16:58:23 -0300

    added new test

--- src/test/get-resource.spec.ts
+++ src/test/get-resource.spec.ts
@@ -90,4 +90,76 @@ describe('core methods', () => {
                 }
             );
     });
+    it(`the resource should have the correct hasOne and hasMany relationships correspondig to the back end response's included resources`,
+    async () => {
+        let test_resource = new TestResource();
+        test_resource.type = 'test_resources';
+        test_resource.id = '1';
+        test_resource.attributes = { name: 'test_name' };
+        test_resource.relationships.test_resource.data = { id: '2', type: 'test_resources' };
+        test_resource.relationships.test_resources.data = [
+            { id: '3', type: 'test_resources' },
+            { id: '4', type: 'test_resources' }
+        ];
+
+        // format has_one relationship to include
+        let test_resource_has_one_relationship =  new TestResource();
+        test_resource_has_one_relationship.type = 'test_resources';
+        test_resource_has_one_relationship.id = '2';
+        test_resource_has_one_relationship.attributes = { name: 'test_name_2' };
+
+        // format has_many relationship to include
+        let test_resource_has_many_relationship_1 =  new TestResource();
+        test_resource_has_many_relationship_1.type = 'test_resources';
+        test_resource_has_many_relationship_1.id = '3';
+        test_resource_has_many_relationship_1.attributes = { name: 'test_name_3' };
+
+        let test_resource_has_many_relationship_2 =  new TestResource();
+        test_resource_has_many_relationship_2.type = 'test_resources';
+        test_resource_has_many_relationship_2.id = '4';
+        test_resource_has_many_relationship_2.attributes = { name: 'test_name_4' };
+
+        let included = [
+            test_resource_has_one_relationship,
+            test_resource_has_many_relationship_1,
+            test_resource_has_many_relationship_2
+        ];
+
+        let test_service = new TestService();
+        test_response_subject.next(new HttpResponse({ body: { data: test_resource, included: included }}));
+
+        await test_service.get('1')
+            .toPromise()
+            .then(
+                (resource) => {
+                    expect(test_resource.type).toBe('test_resources');
+                    expect(test_resource.id).toBe('1');
+                    expect(resource.attributes.name).toBe('test_name');
+                    expect(resource.relationships.test_resource instanceof DocumentResource).toBeTruthy();
+                    expect(resource.relationships.test_resources instanceof DocumentCollection).toBeTruthy();
+                    expect((<DocumentResource>resource.relationships.test_resource).data.id).toBe('2');
+                    expect((<DocumentResource>resource.relationships.test_resource).data.attributes.name).toBe('test_name_2');
+                    expect((<DocumentCollection>resource.relationships.test_resources).data.find(related_resource => related_resource.id === '3'))
+                        .toBeTruthy();
+                    expect(
+                        (<DocumentCollection>resource.relationships.test_resources)
+                            .data
+                            .find(related_resource => related_resource.id === '3')
+                            .attributes
+                            .name
+                        )
+                        .toBe('test_name_3');
+                    expect((<DocumentCollection>resource.relationships.test_resources).data.find(related_resource => related_resource.id === '4'))
+                        .toBeTruthy();
+                    expect(
+                        (<DocumentCollection>resource.relationships.test_resources)
+                            .data
+                            .find(related_resource => related_resource.id === '4')
+                            .attributes
+                            .name
+                        )
+                        .toBe('test_name_4');
+                }
+            );
+    });
 });
