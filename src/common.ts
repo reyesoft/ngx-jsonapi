a28bef2d7e7962929924ac16b0acb2f187a447e8
Maxi <maxi7587@hotmail.com>
2019-07-22 11:59:30 -0300

    Add cachememory management methods (#182)
    * improved cachememory methods
    * added cache methods to core
    * added decorator to check that the services are injected

--- src/common.ts
+++ src/common.ts
@@ -1,4 +1,5 @@
 import { ICacheable } from './interfaces/cacheable';
+import { Core } from './core';
 import { DocumentResource } from './document-resource';
 import { DocumentCollection } from './document-collection';
 
@@ -12,4 +13,38 @@ export function isCollection(document: DocumentResource | DocumentCollection): d
 
 export function isResource(document: DocumentResource | DocumentCollection): document is DocumentResource {
     return 'id' in document.data;
+}
+
+// NOTE: Checks that the service passed to the method is registered (method needs to have service's type or a resource as first arg)
+export function serviceIsRegistered(target: Object, key: string | symbol, descriptor: PropertyDescriptor): PropertyDescriptor|null {
+    const original = descriptor.value;
+
+    descriptor.value = function() {
+        let args = Array.prototype.slice.call(arguments);
+        let type: string;
+        try {
+            if (typeof args[0] === 'string') {
+                type = args[0];
+            } else {
+                type = args[0].type;
+            }
+        } catch (err) {
+            console.warn(`ERROR: First argument of methods decorated with serviceIsRegistered has to be string or Resource.`);
+
+            return null;
+        }
+
+        const service_is_registered = Core.me.getResourceService(type);
+        if (!service_is_registered) {
+            console.warn(`ERROR: ${type} service has not been registered.`);
+
+            return null;
+        }
+
+        const result = original.apply(this, args);
+
+        return result;
+    };
+
+    return descriptor;
 }
