be385cd3e5ab49caf1aa1101c86937207bf3b870
maxi7587 <maxi7587@hotmail.com>
2019-08-13 11:25:03 -0300

    detail

--- src/services/cachememory.ts
+++ src/services/cachememory.ts
@@ -85,6 +85,9 @@ export class CacheMemory<R extends Resource = Resource> {
         this.resources[id].attributes = {}; // just for confirm deletion on view
         // this.resources[id].relationships = {}; // just for confirm deletion on view
         for (let relationship in this.resources[id].relationships) {
+            if (this.resources[id].relationships[relationship].data === null) {
+                continue;
+            }
             if (this.resources[id].relationships[relationship].data.constructor === Array) {
                 this.resources[id].relationships[relationship].data = []; // just in case that there is a for loop using it
             } else if (this.resources[id].relationships[relationship].data.constructor === Object) {
