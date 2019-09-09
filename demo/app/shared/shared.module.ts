248d18261ef1895d860d25575807310f69f54483
Pablo Reyes <pablo@srtk.com>
2019-09-09 09:42:58 -0300

    demo updated (#212)

--- demo/app/shared/shared.module.ts
+++ demo/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CollectionInfoComponent } from './collection-info.component';
+import { ResourceInfoComponent } from './resource-info.component';
 import { CollectionPaginatorComponent } from './collection-paginator.component';
 import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [CommonModule, RouterModule],
-    exports: [CollectionInfoComponent, CollectionPaginatorComponent],
-    declarations: [CollectionInfoComponent, CollectionPaginatorComponent]
+    exports: [CollectionInfoComponent, ResourceInfoComponent, CollectionPaginatorComponent],
+    declarations: [CollectionInfoComponent, ResourceInfoComponent, CollectionPaginatorComponent]
 })
 export class SharedModule {}
