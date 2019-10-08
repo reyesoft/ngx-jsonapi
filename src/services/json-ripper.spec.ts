61bf3dd67fd5e726bc1505b1a1f444a5bf849a7c
maxi7587 <maxi7587@hotmail.com>
2019-10-08 12:50:24 -0300

    fix-test-and-prettier

--- src/services/json-ripper.spec.ts
+++ src/services/json-ripper.spec.ts
@@ -3,131 +3,131 @@ import { JsonRipper } from './json-ripper';
 import { DocumentCollection } from '../document-collection';
 import { TestFactory } from '../tests/factories/test-factory';
 
-// describe('JsonRipper for resources', () => {
-//     let book = TestFactory.getBook('5');
-//     book.attributes.title = 'Fahrenheit 451';
-//     book.addRelationship(TestFactory.getAuthor('2'), 'author');
-//     // @todo maxi: factory dont work?
-//     // book.addRelationship(TestFactory.getPhoto('2'));
-//     // book.addRelationship(TestFactory.getPhoto('1'));
-//
-//     it('A resource is converted to objects for a DataProvider', () => {
-//         let mocked_service_data: { [key: string]: any } = { parseToServer: false };
-//         spyOn(Resource.prototype, 'getService').and.returnValue(mocked_service_data);
-//
-//         let obj = JsonRipper.toResourceElements('some.key', book);
-//         expect(obj.length).toBe(1);
-//         expect(obj[0].key).toBe('some.key');
-//         expect(obj[0].content.data).toMatchObject({
-//             attributes: { title: 'Fahrenheit 451' },
-//             id: '5',
-//             type: 'books',
-//             relationships: {
-//                 author: {
-//                     data: { id: '2', type: 'authors' }
-//                 }
-//             }
-//         });
-//
-//         // hasManyRelationships
-//         // expect(obj[2].content.data.relationships.books.data.length).toBe(2);
-//         // expect(Object.keys(obj[2].content.data.relationships.books.data[0]).length).toBe(2); // id and type
-//     });
-//
-//     it('A resource with include is converted to objects for a DataProvider', () => {
-//         let mocked_service_data: { [key: string]: any } = { parseToServer: false };
-//         spyOn(Resource.prototype, 'getService').and.returnValue(mocked_service_data);
-//
-//         let obj = JsonRipper.toResourceElements('some.key', book, ['author']);
-//         expect(obj.length).toBe(2);
-//         expect(obj[0].key).toBe('some.key');
-//         expect(obj[1].content.data).toMatchObject({
-//             id: '2',
-//             type: 'authors',
-//             attributes: {
-//                 name: /.+/
-//             },
-//             relationships: {}
-//         });
-//     });
-//
-//     it('A ripped resource saved via DataProvider is converted to a Json', async done => {
-//         let mocked_service_data: { [key: string]: any } = { parseToServer: false };
-//         spyOn(Resource.prototype, 'getService').and.returnValue(mocked_service_data);
-//
-//         let jsonRipper = new JsonRipper();
-//         await jsonRipper.saveResource(book);
-//         let json = await jsonRipper.getResource(JsonRipper.getResourceKey(book));
-//         expect(json.data).toMatchObject({
-//             attributes: { title: /.+/ },
-//             id: '5',
-//             type: 'books',
-//             relationships: {
-//                 author: {
-//                     data: { id: /.+/, type: 'authors' }
-//                 }
-//             }
-//         });
-//
-//         done();
-//     }, 500);
-//
-//     it('A ripped resource maintain cache_last_update property', async () => {
-//         let mocked_service_data: { [key: string]: any } = { parseToServer: false };
-//         spyOn(Resource.prototype, 'getService').and.returnValue(mocked_service_data);
-//
-//         let jsonRipper = new JsonRipper();
-//         await jsonRipper.saveResource(book);
-//         let json = await jsonRipper.getResource(JsonRipper.getResourceKey(book));
-//         expect(json.data.cache_last_update).toBeGreaterThanOrEqual(Date.now() - 100);
-//     });
-//
-//     it('A ripped resource with include saved via DataProvider is converted to a Json', async done => {
-//         let mocked_service_data: { [key: string]: any } = { parseToServer: false };
-//         spyOn(Resource.prototype, 'getService').and.returnValue(mocked_service_data);
-//
-//         let jsonRipper = new JsonRipper();
-//         await jsonRipper.saveResource(book, ['author']);
-//         let json = await jsonRipper.getResource(JsonRipper.getResourceKey(book), ['author']);
-//         expect(json.included.length).toEqual(1);
-//         expect(json.included[0]).toMatchObject({
-//             id: '2',
-//             type: 'authors',
-//             attributes: {},
-//             relationships: {}
-//         });
-//
-//         done();
-//     }, 500);
-//
-//     it('A ripped resource with hasOne = null saved via DataProvider is converted to a Json', async () => {
-//         let mocked_service_data: { [key: string]: any } = { parseToServer: false };
-//         spyOn(Resource.prototype, 'getService').and.returnValue(mocked_service_data);
-//
-//         let jsonRipper = new JsonRipper();
-//         book.relationships.author.data = null;
-//         await jsonRipper.saveResource(book, ['author']);
-//         let json = await jsonRipper.getResource(JsonRipper.getResourceKey(book), ['author']);
-//         expect(json.included.length).toEqual(0);
-//         expect(json.data.relationships.author.data).toEqual(null);
-//         // expect(json.included[0]).toMatchObject({
-//         //     id: '2',
-//         //     type: 'authors',
-//         //     attributes: {},
-//         //     relationships: {}
-//         // });
-//     });
-//
-//     it('Requesting DataProvider not cached resource thrown an error', done => {
-//         let jsonRipper = new JsonRipper();
-//         jsonRipper
-//             .getResource('extrange_type.id')
-//             .then()
-//             .catch(data => {
-//                 done();
-//             });
-//     }, 50);
-// });
+describe('JsonRipper for resources', () => {
+    let book = TestFactory.getBook('5');
+    book.attributes.title = 'Fahrenheit 451';
+    book.addRelationship(TestFactory.getAuthor('2'), 'author');
+    // @todo maxi: factory dont work?
+    // book.addRelationship(TestFactory.getPhoto('2'));
+    // book.addRelationship(TestFactory.getPhoto('1'));
+
+    it('A resource is converted to objects for a DataProvider', () => {
+        let mocked_service_data: { [key: string]: any } = { parseToServer: false };
+        spyOn(Resource.prototype, 'getService').and.returnValue(mocked_service_data);
+
+        let obj = JsonRipper.toResourceElements('some.key', book);
+        expect(obj.length).toBe(1);
+        expect(obj[0].key).toBe('some.key');
+        expect(obj[0].content.data).toMatchObject({
+            attributes: { title: 'Fahrenheit 451' },
+            id: '5',
+            type: 'books',
+            relationships: {
+                author: {
+                    data: { id: '2', type: 'authors' }
+                }
+            }
+        });
+
+        // hasManyRelationships
+        // expect(obj[2].content.data.relationships.books.data.length).toBe(2);
+        // expect(Object.keys(obj[2].content.data.relationships.books.data[0]).length).toBe(2); // id and type
+    });
+
+    it('A resource with include is converted to objects for a DataProvider', () => {
+        let mocked_service_data: { [key: string]: any } = { parseToServer: false };
+        spyOn(Resource.prototype, 'getService').and.returnValue(mocked_service_data);
+
+        let obj = JsonRipper.toResourceElements('some.key', book, ['author']);
+        expect(obj.length).toBe(2);
+        expect(obj[0].key).toBe('some.key');
+        expect(obj[1].content.data).toMatchObject({
+            id: '2',
+            type: 'authors',
+            attributes: {
+                name: /.+/
+            },
+            relationships: {}
+        });
+    });
+
+    it('A ripped resource saved via DataProvider is converted to a Json', async done => {
+        let mocked_service_data: { [key: string]: any } = { parseToServer: false };
+        spyOn(Resource.prototype, 'getService').and.returnValue(mocked_service_data);
+
+        let jsonRipper = new JsonRipper();
+        await jsonRipper.saveResource(book);
+        let json = await jsonRipper.getResource(JsonRipper.getResourceKey(book));
+        expect(json.data).toMatchObject({
+            attributes: { title: /.+/ },
+            id: '5',
+            type: 'books',
+            relationships: {
+                author: {
+                    data: { id: /.+/, type: 'authors' }
+                }
+            }
+        });
+
+        done();
+    }, 500);
+
+    it('A ripped resource maintain cache_last_update property', async () => {
+        let mocked_service_data: { [key: string]: any } = { parseToServer: false };
+        spyOn(Resource.prototype, 'getService').and.returnValue(mocked_service_data);
+
+        let jsonRipper = new JsonRipper();
+        await jsonRipper.saveResource(book);
+        let json = await jsonRipper.getResource(JsonRipper.getResourceKey(book));
+        expect(json.data.cache_last_update).toBeGreaterThanOrEqual(Date.now() - 100);
+    });
+
+    it('A ripped resource with include saved via DataProvider is converted to a Json', async done => {
+        let mocked_service_data: { [key: string]: any } = { parseToServer: false };
+        spyOn(Resource.prototype, 'getService').and.returnValue(mocked_service_data);
+
+        let jsonRipper = new JsonRipper();
+        await jsonRipper.saveResource(book, ['author']);
+        let json = await jsonRipper.getResource(JsonRipper.getResourceKey(book), ['author']);
+        expect(json.included.length).toEqual(1);
+        expect(json.included[0]).toMatchObject({
+            id: '2',
+            type: 'authors',
+            attributes: {},
+            relationships: {}
+        });
+
+        done();
+    }, 500);
+
+    it('A ripped resource with hasOne = null saved via DataProvider is converted to a Json', async () => {
+        let mocked_service_data: { [key: string]: any } = { parseToServer: false };
+        spyOn(Resource.prototype, 'getService').and.returnValue(mocked_service_data);
+
+        let jsonRipper = new JsonRipper();
+        book.relationships.author.data = null;
+        await jsonRipper.saveResource(book, ['author']);
+        let json = await jsonRipper.getResource(JsonRipper.getResourceKey(book), ['author']);
+        expect(json.included.length).toEqual(0);
+        expect(json.data.relationships.author.data).toEqual(null);
+        // expect(json.included[0]).toMatchObject({
+        //     id: '2',
+        //     type: 'authors',
+        //     attributes: {},
+        //     relationships: {}
+        // });
+    });
+
+    it('Requesting DataProvider not cached resource thrown an error', done => {
+        let jsonRipper = new JsonRipper();
+        jsonRipper
+            .getResource('extrange_type.id')
+            .then()
+            .catch(data => {
+                done();
+            });
+    }, 50);
+});
 
 describe('JsonRipper for collections', () => {
     let authors = new DocumentCollection();
