99a96bf9b88bd671d22976bdfc8580f29e2b6ece
maxi7587 <maxi7587@hotmail.com>
2019-10-10 14:39:33 -0300

    fix-to-object-empty-relationships

--- src/core.ts
+++ src/core.ts
@@ -98,7 +98,9 @@ export class Core {
     public getResourceServiceOrFail(type: string): Service {
         let service = this.resourceServices[type];
         if (!service) {
-            throw new Error('The requested service has not been registered, please use register() method or @Autoregister() decorator');
+            throw new Error(
+                `The requested service with type ${type} has not been registered, please use register() method or @Autoregister() decorator`
+            );
         }
 
         return service;
