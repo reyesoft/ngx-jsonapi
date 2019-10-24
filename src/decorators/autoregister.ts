b9f5465304cdf86280fc381f4b053b3b26511d7f
maxi7587 <maxi7587@hotmail.com>
2019-10-24 11:33:43 -0300

    removed-warning

--- src/decorators/autoregister.ts
+++ src/decorators/autoregister.ts
@@ -2,7 +2,5 @@
  * @deprecated since version 3.0.0
  */
 export function Autoregister() {
-    return (target): any => {
-        console.warn(`@Autoregister() decorator has been deprecated (as it's no longer needed) and will be removed in v3.0.0.`);
-    };
+    return (target): any => { /**/ };
 }
